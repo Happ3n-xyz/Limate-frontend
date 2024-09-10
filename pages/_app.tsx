d438216030fad0bb2a46cf8288affcf5c1363fee
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-09-09 20:45:07 -0500

    feat: adding Minato and Fuji networks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { celoAlfajores } from "wagmi/chains";
+import { avalancheFuji, sepolia} from "wagmi/chains";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -12,14 +12,43 @@ import theme from "../theme";
 import { Toaster } from "react-hot-toast";
 import { UserProvider } from "../context/userContext";
 import Layout from "../components/Layout";
+import { defineChain } from "viem";
+
+export const minato = /*#__PURE__*/ defineChain({
+  id: 1946,
+  name: 'Minato',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Sepolia Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.minato.soneium.org'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://explorer-testnet.soneium.org',
+      apiUrl: 'https://explorer-testnet.soneium.org/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0x6E59Bf419a87FE1EA3AB97029c44568F6689aD5e',
+      blockCreated: 1369034,
+    },
+  },
+  testnet: true,
+})
 
 const config = getDefaultConfig({
   appName: "RainbowKit App",
   projectId: process.env.NEXT_PUBLIC_PROYECT_ID ?? "",
   chains: [
-    celoAlfajores,
+    avalancheFuji,
+    minato,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-      ? [celoAlfajores]
+      ? [avalancheFuji]
       : []),
   ],
   ssr: true,
