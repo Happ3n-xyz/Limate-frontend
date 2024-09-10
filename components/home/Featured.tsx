c0f1fb458ce81b80e82b3cb405fd9e405cdc4ab3
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-09-09 20:15:53 -0500

    feat: adding intro page

---
+++ components/home/Featured.tsx
@@ -0,0 +1,71 @@
+import React, { Fragment, useEffect, useState } from "react";
+import {
+  Box,
+  Text,
+  Button,
+  Grid,
+  Center,
+  Flex,
+  useTheme,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useAccount } from 'wagmi'
+import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useRouter } from "next/router";
+
+const Featured = () => {
+  const { isConnected } = useAccount()
+  const router = useRouter()
+  if (isConnected) {
+    //go to myHaps page
+    router.push("/myHaps");
+  }
+
+  return (
+    <Fragment>
+      <Box mt={"100px"} w={"100%"} h={'100%'}>
+        <Center mb={10} justifyContent={"center"} w={"100%"}>
+          <Image
+            src={"images/logo.svg"}
+            alt="happ3n logo"
+            height={400}
+            width={400}
+          />
+        </Center>
+        <Center justifyContent={"center"}>
+          <Text fontSize="60px" fontWeight="bold" textAlign="center" w={"50%"}>
+            {"Join Limate And enjoy meeting mates in your events"}
+          </Text>
+        </Center>
+        <Center mt={16}>
+          <ConnectButton.Custom>
+            {({ openConnectModal }) => (
+              <Button
+                onClick={openConnectModal}
+                color={'textPrimary'}
+                bg="transparent"
+                border="1px"
+                borderColor={'gray.600'}
+                borderRadius="md"
+                _hover={{ bg: "gray.600" }}
+              >
+                <Box as="span" mr={6}>
+                  <Image
+                    src="/images/WalletIcon.svg"
+                    alt="Wallet Icon"
+                    width={24}
+                    height={24}
+                  />
+                </Box>
+                Connect Wallet
+              </Button>
+            )}
+          </ConnectButton.Custom>
+        </Center>
+      </Box>
+    </Fragment>
+  );
+};
+
+export default Featured;
---
+++ pages/_app.tsx
@@ -0,0 +1,49 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { celoAlfajores } from "wagmi/chains";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../theme";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "../context/userContext";
+import Layout from "../components/Layout";
+
+const config = getDefaultConfig({
+  appName: "RainbowKit App",
+  projectId: process.env.NEXT_PUBLIC_PROYECT_ID ?? "",
+  chains: [
+    celoAlfajores,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+      ? [celoAlfajores]
+      : []),
+  ],
+  ssr: true,
+});
+
+const client = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider>
+          <ChakraProvider theme={theme}>
+          <UserProvider>
+            <Layout>
+              <Component {...pageProps} />
+              <Toaster />
+            </Layout>
+          </UserProvider>
+          </ChakraProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;
